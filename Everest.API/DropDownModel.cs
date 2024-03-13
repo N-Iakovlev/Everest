041aefca790fc752cc688f566b29b56411f680c8
nikolay <ne.iakovlev@gmail.com>
2024-03-13 08:57:40 +0300

    addDDModel

---
+++ Everest.API/DropDownModel.cs
@@ -0,0 +1,15 @@
+﻿using Incoding.Core.ViewModel;
+
+namespace Everest.API
+{
+    public class DropDownModel
+    {
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+
+        public string Template { get; set; }
+
+        public List<KeyValueVm> Items { get; set; }
+    }
+}

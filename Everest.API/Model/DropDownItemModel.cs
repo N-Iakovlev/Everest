026d252039e6e4c4e0767a96bd00a20c30fdb8c6
nikolay <ne.iakovlev@gmail.com>
2024-03-26 11:40:03 +0300

    ButtonRepeir

--- Everest.API/Model/DropDownItemModel.cs
+++ Everest.API/Model/DropDownItemModel.cs
@@ -4,8 +4,28 @@ namespace Everest.API;
 public class DropDownItemModel
 {
     // Значение элемента списка
-    public string Value { get; set; }
+    public object Value { get; set; }
 
     // Текст, отображаемый для элемента списка
     public string Text { get; set; }
+    public string Search { get; set; }
+
+    public bool? Selected { get; set; }
+
+    public DropDownItemModel(object value, string text, bool selected = false)
+    {
+        this.Value = value;
+        this.Text = text;
+        this.Selected = selected;
+    }
+
+    public static implicit operator KeyValueVm(DropDownItemModel item)
+    {
+        return new KeyValueVm(item.Value, item.Text);
+    }
+
+    public static implicit operator DropDownItemModel(KeyValueVm item)
+    {
+        return new DropDownItemModel(item.Value, item.Text);
+    }
 }
